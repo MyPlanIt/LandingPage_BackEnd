64aff5822857b056a0c69904d855f69a5e02d2ee
ssssujini99 <sssssjin99@naver.com>
2022-03-01 22:13:42 +0900

    docker settings 수정 12

--- myplanit/settings/base.py
+++ myplanit/settings/base.py
@@ -16,7 +16,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.0:8000']
+ALLOWED_HOSTS = []
 
 
 # Application definition
