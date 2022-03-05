91cf5c383ae5c266427fbabc7c711a8f52991cc2
ssssujini99 <sssssjin99@naver.com>
2022-03-05 11:55:21 +0900

    change debug mode

--- myplanit/settings/base.py
+++ myplanit/settings/base.py
@@ -14,7 +14,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'http://ec2-3-39-42-138.ap-northeast-2.compute.amazonaws.com', '3.39.42.138']
 
